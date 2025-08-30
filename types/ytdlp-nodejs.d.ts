aedc134a87a95c8061d9d48aae7eb41ba28973bf
heejun0402 <heejun0402@gmail.com>
2025-08-30 22:45:16 +0900

    제발

--- types/ytdlp-nodejs.d.ts
+++ types/ytdlp-nodejs.d.ts
@@ -2,10 +2,17 @@ declare module 'ytdlp-nodejs' {
   export class YtDlp {
     constructor(options?: { binaryPath?: string; ffmpegPath?: string });
     checkInstallationAsync(options?: { ffmpeg?: boolean }): Promise<boolean>;
+    checkInstallation(options?: { ffmpeg?: boolean }): boolean;
     getInfoAsync(url: string): Promise<VideoInfo | PlaylistInfo>;
     downloadAsync(url: string, options?: DownloadOptions): Promise<any>;
     getTitleAsync(url: string): Promise<string>;
     getThumbnailsAsync(url: string): Promise<VideoThumbnail[]>;
+    getFileAsync(url: string, options?: FileDownloadOptions): Promise<File>;
+    execAsync(url: string, options?: any): Promise<any>;
+    stream(url: string, options?: any): StreamResult;
+    download(url: string, options?: DownloadOptions): any;
+    exec(url: string, options?: any): any;
+    downloadFFmpeg(): Promise<void>;
   }
 
   interface VideoInfo {
@@ -38,6 +45,28 @@ declare module 'ytdlp-nodejs' {
     output?: string;
     mergeOutputFormat?: string;
     onProgress?: (progress: any) => void;
+    [key: string]: any;
+  }
+
+  interface FileDownloadOptions {
+    format?: {
+      filter?: string;
+      type?: string;
+      quality?: string;
+    };
+    filename?: string;
+    onProgress?: (progress: any) => void;
+    [key: string]: any;
+  }
+
+  interface StreamResult {
+    pipe(destination: any): any;
+    pipeAsync(destination: any): Promise<any>;
+  }
+
+  interface File {
+    buffer: Buffer;
+    filename: string;
     [key: string]: any;
   }
 }
